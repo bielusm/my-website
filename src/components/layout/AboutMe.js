051d9d9ab1313a7bb5dee0a4ccef5ea700ac9852
Mitchell Bielus <mitchellbielus@gmail.com>
2020-04-16 13:46:06 -0500

    updated responsiveness

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -7,29 +7,21 @@ const AboutMe = ({ className }) => {
       <h1 id="aboutMe">About Me:</h1>
 
       <Container>
-        <Row>
-          <Col>
-            <Row>
-              <div id="profilePicture"></div>
-            </Row>
-            <Row>
-              <center className="profiletext">
-                <p>
-                  Mitchell Bielus
-                  <br />
-                  Web-Developer and Programmer
-                </p>
-              </center>
-            </Row>
-          </Col>
-          <Col>
-            <h3>
-              My name is Mitchell Bielus and I am a web developer and
-              programmer. My primary language is JavaScript, but I'm also
-              interested in learning new languages outside of my comfort zone
-            </h3>
-          </Col>
-        </Row>
+        <div id="profilePicture"></div>
+
+        <center className="profiletext">
+          <p>
+            Mitchell Bielus
+            <br />
+            Web-Developer and Programmer
+          </p>
+        </center>
+
+        <h3>
+          My name is Mitchell Bielus and I am a web developer and programmer. My
+          primary language is JavaScript, but I'm also interested in learning
+          new languages outside of my comfort zone
+        </h3>
       </Container>
     </div>
   );
