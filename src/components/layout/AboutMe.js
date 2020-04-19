ce5a46e62fe26203c4fe648cbe9d544001cba2df
Mitchell Bielus <mitchellbielus@gmail.com>
2020-04-19 12:39:05 -0500

    updated fonts

--- src/components/layout/AboutMe.js
+++ src/components/layout/AboutMe.js
@@ -2,24 +2,22 @@ import React from 'react';
 
 const AboutMe = ({ className }) => {
   return (
-    <div className={className}>
-      <h1 id="about-me">About Me:</h1>
-
+    <div className={className} id="about-me">
       <div id="profilePicture"></div>
 
       <center className="profiletext">
-        <h3>Mitchell Bielus</h3>
+        <h1>Mitchell Bielus</h1>
         <h3> Web-Developer and Programmer</h3>
         <h3>
           <a href="/assets/web resume.pdf">Resume</a>
         </h3>
       </center>
 
-      <h4>
+      <p>
         My name is Mitchell Bielus and I am a web developer and programmer. My
         primary language is JavaScript, but I'm also interested in learning new
         languages outside of my comfort zone.
-      </h4>
+      </p>
     </div>
   );
 };
