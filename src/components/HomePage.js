3772abfa6db87c6db5246ec5b3ab2bfbb9c9c443
Mitchell Bielus <mitchellbielus@gmail.com>
2020-04-20 15:07:34 -0500

    updated mobile button responsiveness

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import TopNav from './layout/TopNav';
 import Contact from './layout/Contact';
-import { Container } from 'reactstrap';
 import AboutMe from './layout/AboutMe';
 import Projects from './layout/Projects';
 import { Scrollspy } from 'reactstrap-scrollspy';
